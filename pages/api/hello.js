34a55747c0be1811b859b20c35335e737acc2763
Onuj1234 <onuj1234@Onuj1234s-MacBook-Pro.local>
2021-09-01 21:51:31 +0900

    Add api

---
+++ pages/api/hello.js
@@ -0,0 +1,8 @@
+import { getPostData } from "../../lib/posts"
+
+// api executed in server, lib like 'fs' or accessing backend files are possible.
+// Don't call api in getStatic (fetch localhost:3000/api/...), instead, copy and paste the logic inside.
+export default async function handler(req, res) {
+    const postData = await getPostData("ssg-ssr")
+    res.status(200).json(postData)
+}
